2461514ccb96950c867bd0da35074010d3f1700b
jimgar <jimmahgee@gmail.com>
2024-08-19 21:33:25 +0100

    Wider net for mounted volumes in docker run

--- build.sh
+++ build.sh
@@ -1,18 +1,14 @@
-
 #!/bin/sh
-docker stop pbs && docker container rm pbs
+docker stop py-deb && docker container rm py-deb
 
-# Build the developer image
-docker build -t pbs:latest .
+docker build -t py-deb:latest .
 
 docker run \
     -p 9000:8000 \
     -it \
     --rm \
     -v ~/.aws/:/root/.aws \
-    -v ./requirements.txt:/project/requirements.txt \
-    -v ./matplotlib-demo.qmd:/project/matplotlib-demo.qmd \
-    -v ./qp.sh:/project/qp.sh \
-    --name pbs \
-    pbs:latest \
+    -v ./:/project \
+    --name py-deb \
+    py-deb:latest \
     bash
