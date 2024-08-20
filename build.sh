d3ee2ab2bb3156ded97f62e13360a5c20674792a
jimgar <jim@policyinpractice.co.uk>
2024-08-20 11:29:09 +0100

    More secure container port access

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ docker stop py-deb && docker container rm py-deb
 docker build -t py-deb:latest .
 
 docker run \
-    -p 9000:8000 \
+    -p 127.0.0.1:9000:8000 \
     -it \
     --rm \
     -v ~/.aws/:/root/.aws \
